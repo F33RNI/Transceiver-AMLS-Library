61b965f1c40103f438c9fb73001f4863306f4c30
Frey Hertz <39090123+XxOinvizioNxX@users.noreply.github.com>
2021-04-26 19:27:13 -0500

    Update Transceiver.h

--- Transceiver.h
+++ Transceiver.h
@@ -1,28 +1,4 @@
-/*
-  *********************************************************
-  *                                                       *
-  * UART Transceiver for Windows and Linux (Raspberry PI) *
-  *                                                       *
-  ********************************************************************************************
-  *
-  * COPYRIGHT(c) 2020 Pavel Evgenyevich Neshumov
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *
-  * THIS SOFTWARE WAS DEVELOPED AS PART OF PROJECT ACTIVITY IN GROUP 181-311 AT THE
-  * MOSCOW POLYTECHNIC UNIVERCITY. HOWEVER, THE OWNER (Pavel Evgenyevich Neshumov)
-  * REMAINS THE RIGHT, WITHOUT NOTICE, TO CHANGE THE PURPOSE AND ENVIRONMENT OF APPLICATION
-  * OF THIS SOFTWARE. ALSO THE OWNER (Pavel Evgenyevich Neshumov) MAY PROHIBIT USE OF THIS
-  * SOFTWARE FOR ANY ORGANIZATION, PEOPLE, STRUCTURES.
-  *
-  ********************************************************************************************
-*/
+
 
 #pragma once
 #ifndef TRANSCEIVER_H
